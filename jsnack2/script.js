99e85926170f38607f858e41c5270acf837e3fa3
michele-pavlenco <michelepavlenco.mp@gmail.com>
2022-03-31 16:57:56 +0200

    added-partof2

---
+++ jsnack2/script.js
@@ -0,0 +1,41 @@
+//Crea un array vuoto.
+//Chiedi per 6 volte all’utente di inserire un numero,
+//se è dispari inseriscilo nell’array.
+alert ("Ciao, inserisci sei numeri uno alla volta")
+/*let num1 = prompt ("Primo numero")
+let num2 = prompt ("Secondo numero")
+let num3 = prompt ("terzo numero")
+let num4 = prompt ("Quarto numero")
+let num5 = prompt ("Quinto numero")
+let num6 = prompt ("Sesto numero")*/
+const div = document.querySelector("div.app")
+let nums = []
+let num = []
+
+for(let i = 0; i< 6; i++){
+    let nums = prompt("Scegli un numero")
+    nums.push
+    console.log(nums)
+}
+
+    if(!(nums % 2 === 0)){
+        num.push
+        div.append
+        
+        
+
+    }
+
+    console.log(num)
+
+
+
+
+
+
+
+
+
+
+//num.push
+//console.log(num)
